4ae0881236358f0d1939a8b5226e4632dba2dbf6
mohammedsaleh111 <salehhamada664@gmail.com>
2024-12-23 16:11:00 +0200

    new user2

--- app/registration/page.tsx
+++ app/registration/page.tsx
@@ -140,8 +140,8 @@ const RegistrationPage = () => {
     const notificationData = {
       app_id: oneSignalAppId,
       included_segments: ["All"], // أو استخدم include_player_ids لاستهداف جهاز معين
-      headings: { ar: " مستخدم جديد " },
-      contents: { ar: ` المسيخدم الجديد , ${userName}, يطلب التواصل .` },
+      headings: { en: " مستخدم جديد " },
+      contents: { en: ` المسيخدم الجديد , ${userName}, يطلب التواصل .` },
     };
   
     try {

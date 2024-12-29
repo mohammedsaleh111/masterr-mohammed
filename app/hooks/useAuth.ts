3d5242617f5c7e64a3263a44a5910dcb3fa8b99c
mohammedsaleh111 <salehhamada664@gmail.com>
2024-12-29 16:38:23 +0200

    fix popup

--- app/hooks/useAuth.ts
+++ app/hooks/useAuth.ts
@@ -1,7 +1,7 @@
 // hooks/useAuth.ts
 import { useEffect, useState } from "react";
 import { auth, googleProvider } from "../../lib/firebase";
-import { signInWithPopup } from "firebase/auth";
+import { signInWithPopup, signInWithRedirect } from "firebase/auth";
 
 export function useAuth() {
   interface User {
@@ -27,7 +27,7 @@ export function useAuth() {
   // دالة لتسجيل الدخول باستخدام جوجل
   const signInWithGoogle = async () => {
     try {
-      await signInWithPopup(auth, googleProvider);
+      await signInWithRedirect(auth, googleProvider);
     } catch (error) {
       console.error("Error signing in:", error);
     }

ebed44d5f7eeec920308ab3d4ef6c4a93556cd54
mohammedsaleh111 <salehhamada664@gmail.com>
2025-01-02 00:48:55 +0200

    seo optimization

--- next.config.js
+++ next.config.js
@@ -3,6 +3,23 @@ const nextConfig = {
     images: {
         domains: ['lh3.googleusercontent.com'],
       },
+      reactStrictMode: true,
+      experimental: {
+        appDir: true,
+      },
+      async headers() {
+        return [
+          {
+            source: '/(.*)',
+            headers: [
+              {
+                key: 'X-Robots-Tag',
+                value: 'index, follow',
+              },
+            ],
+          },
+        ];
+      },
 }
 
 module.exports = nextConfig
